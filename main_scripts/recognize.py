f542034b1080f71f2471de052ce43b096260e493
abhimanyu-bitsgoa <abhimanyu.bitsgoa@gmail.com>
2018-03-26 16:39:04 +0000

    Adding scanFace module

--- main_scripts/recognize.py
+++ main_scripts/recognize.py
@@ -2,55 +2,59 @@
 import face_recognition
 import picamera
 import numpy as np
+import os
 
-# Get a reference to the Raspberry Pi camera.
-# If this fails, make sure you have a camera connected to the RPi and that you
-# enabled your camera in raspi-config and rebooted first.
-camera = picamera.PiCamera()
-camera.resolution = (320, 240)
-output = np.empty((240, 320, 3), dtype=np.uint8)
-print(" I have just intialized all the camera")
-# print ("printing array before")
-# print (output)
-# Load a sample picture and learn how to recognize it.
-# print("Loading known face image(s)")
-# obama_image = face_recognition.load_image_file("abhi.jpg")
-# obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-# npar=np.frombuffer(obama_face_encoding)
+ENCODING_PATH="../imageEncodings/"
 
-# print("Obama raw image")
-# print(obama_image)
+def loadDataset(dataset,nameset):
+    directory = os.fsencode(ENCODING_PATH)
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".npy"):
+            dataset.append(np.load(os.path.join(ENCODING_PATH, filename)))
+            nameset.append(filename[:-4])
 
-# print("Obama face encoding")
-# print(obama_face_encoding)
-# print("Numpy encoding of Obama")
-# print(npar)
-# np.save('test3.npy',npar)
-d = np.load('test3.npy')
-print("I have loaded the array")
 
-# Initialize some variables
-face_locations = []
-face_encodings = []
 
-while True:
-    print("Capturing image.")
-    # Grab a single frame of video from the RPi camera as a numpy array
-    camera.capture(output, format="rgb")
 
-    # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(output)
-    print("Found {} faces in image.".format(len(face_locations)))
-    face_encodings = face_recognition.face_encodings(output, face_locations)
-    print("live face encodings")
-    print(face_encodings)
-    # Loop over each face found in the frame to see if it's someone we know.
-    for face_encoding in face_encodings:
-        # See if the face is a match for the known face(s)
-        match = face_recognition.compare_faces([d], face_encoding)
-        name = "<Unknown Person>"
+def scanFace():
+    camera = picamera.PiCamera()
+    camera.resolution = (320, 240)
+    output = np.empty((240, 320, 3), dtype=np.uint8)
+    print("Loading known face image(s)")
+    # obama_image = face_recognition.load_image_file("abhi.jpg")
+    # obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+    # npar=np.frombuffer(obama_face_encoding)
+    # np.save('test3.npy',npar)
+    # d = np.load('test3.npy')
 
-        if match[0]:
-            name = "Barack Obama"
+    # Initialize some variables
+    face_locations = []
+    face_encodings = []
+    dataset=[]
+    nameset=[]
+    loadDataset(dataset,nameset)
+    print("Please face the camera")
+    while True:
+        # Grab a single frame of video from the RPi camera as a numpy array
+        camera.capture(output, format="rgb")
 
-        print("I see someone named {}!".format(name))
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(output)
+        # print("Found {} faces in image.".format(len(face_locations)))
+        face_encodings = face_recognition.face_encodings(output, face_locations)
+        # Loop over each face found in the frame to see if it's someone we know.
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            match = face_recognition.compare_faces(dataset, face_encoding)
+            scannedName = "#"
+            for name,status in zip(nameset,match):
+                if status==True:
+                    scannedName = name
+                    break
+
+            if scannedName!='#':
+                print(scannedName)
+                # return scannedName
+            # print("I see someone named {}!".format(name))
+scanFace()
