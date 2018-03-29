f4e309af5d6891ab2a76b7d0ee8236240a3516d0
abhimanyu-bitsgoa <abhimanyu.bitsgoa@gmail.com>
2018-03-29 11:28:58 +0000

    Added modules News and Dictionary module to listen.py

--- main_scripts/headlines.py
+++ main_scripts/headlines.py
@@ -3,7 +3,7 @@ from termcolor import colored
 from unidecode import unidecode
 
 
-def getHeadlines(source):
+def getHeadlines():
     source="google-news-in"
     newsSource =source
     url = "https://newsapi.org/v2/top-headlines?apiKey=2ff80ad49a294ffc88827b9f0b47cb6c&sources=" + newsSource
@@ -14,11 +14,14 @@ def getHeadlines(source):
         for article in articles:
             print(colored(unidecode(article["title"]),'green'))
             if article["description"] is not None:
-                print(unidecode(article["description"]))
-                print(colored(article["url"],'yellow'))
-                print(" ")
+                headline=unidecode(article["description"])
+                print(headline)
+                return headline
+                #print(colored(article["url"],'yellow'))
+                #print(" ")
     else:
-        print("Invalid news source.")
+        print("No news today")
+        return "No news today"
 
 if __name__ == '__main__':
-    getHeadlines("google-news-in")
+    getHeadlines()
--- main_scripts/listen.py
+++ main_scripts/listen.py
@@ -5,51 +5,57 @@ from read_card import scanCard
 from jokes import joke
 from demo_gps import getAddress, getTimestamp
 from send_data import sendDb 
+from dictionary import findMeaning
+from headlines import getHeadlines
 
 def recordAttendance(emplid):
-	address=getAddress()
-	time=getTimestamp()
-	sendDb(emplid,address,time)
+        address=getAddress()
+        time=getTimestamp()
+        sendDb(emplid,address,time)
 
 def listenCommands():
-		# obtain audio from the microphone
-		sp.say("How can I help you?")
-		print("How can I help you?")
-		r = sr.Recognizer()
-		message='#'
-		while True:
-				with sr.Microphone(None,48000,1024) as source:
-					# print("How can I help you?")
-					audio = r.listen(source,None,3,None)
-				try:
-					message=r.recognize_google(audio)
-					print(message)
-					if message=='read face':
-						sp.say("Starting Camera, please wait")
-						print("Read face!")
-						emplid=scanFace()
-						recordAttendance(emplid)
-						break
-					elif message=='read card':
-						sp.say("Starting card reader, please wait")
-						print("Read card!")
-						emplid=scanCard()
-						recordAttendance(emplid)
-						break
-					elif message=='joke':
-						jokeText=joke()
-						print(jokeText)
-						sp.say(jokeText)
-					elif message=='exit':
-						sp.say("Bye")
-						print("Bye!")
-						return
-					else:
-						sp.say("Invalid command")
-						print("Invalid command")
-				except sr.UnknownValueError:
-						sp.say("I beg your pardon!")
-						print("I beg your pardon!")
-				except sr.RequestError as e:
-						sp.say("Problem with network")
-						print("Problem with network")
+                # obtain audio from the microphone
+                sp.say("How can I help you?")
+                print("How can I help you?")
+                r = sr.Recognizer()
+                message='#'
+                while True:
+                                with sr.Microphone(None,48000,1024) as source:
+                                        # print("How can I help you?")
+                                        audio = r.listen(source,None,3,None)
+                                try:
+                                        message=r.recognize_google(audio)
+                                        print(message)
+                                        if message=='read face':
+                                                sp.say("Starting Camera, please wait")
+                                                print("Read face!")
+                                                emplid=scanFace()
+                                                recordAttendance(emplid)
+                                                break
+                                        elif message=='read card':
+                                                sp.say("Starting card reader, please wait")
+                                                print("Read card!")
+                                                emplid=scanCard()
+                                                recordAttendance(emplid)
+                                                break
+                                        elif message=='joke':
+                                                jokeText=joke()
+                                                print(jokeText)
+                                                sp.say(jokeText)
+                                        elif message=='news':
+                                                news=getHeadlines()
+                                                print(news)
+                                                sp.say(news)
+                                        elif message=='exit':
+                                                sp.say("Bye")
+                                                print("Bye!")
+                                                return
+                                        else:
+                                                sp.say("Invalid command")
+                                                print("Invalid command")
+                                except sr.UnknownValueError:
+                                                sp.say("I beg your pardon!")
+                                                print("I beg your pardon!")
+                                except sr.RequestError as e:
+                                                sp.say("Problem with network")
+                                                print("Problem with network")
